7eda3e1ae399f7ec63d14b21d111d3a96a6f4f6e
ISNET\berkay.yucel <berkay.yucel@is.net.tr>
2022-06-23 22:58:15 +0300

    update

--- src/views/GameOver.ts
+++ src/views/GameOver.ts
@@ -3,11 +3,11 @@ import { View } from "./common/View";
 
 export class GameOver extends View<Game, GameProps> {
   template(): string {
+    const winner = this.model.get("winner");
+    const score = this.model.get("score");
     return `
     <div> 
-      <h3>${this.model.get("winner")} Win! Your score is ${this.model.get(
-      "score"
-    )}
+      <h3>${winner} Win! Your score is ${score}
     </h3>
     </div>`;
   }
